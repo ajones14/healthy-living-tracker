75bc7ecdca4b4500b5683b8db51f1fddced845f2
Abigail Wiggins <rebtaylor14@gmail.com>
2020-02-04 14:35:53 -0600

    Intial commit and basic setup

---
+++ src/main/java/org/launchcode/healthylivingtracker/HealthyLivingTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.healthylivingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLivingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyLivingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/healthylivingtracker/HealthyLivingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.healthylivingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyLivingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
