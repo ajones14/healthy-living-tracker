2c1983c42747f31f5482f5c1f19d6f9125953f3d
Abigail Wiggins <rebtaylor14@gmail.com>
2020-02-17 03:38:36 -0600

    Adding add food form

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,29 @@
+function init () {
+
+    let addBreakfastButton = document.getElementById("addBreakfast");
+    let addLunchButton = document.getElementById("addLunch");
+    let addDinnerButton = document.getElementById("addDinner");
+    let addSnackButton = document.getElementById("addSnack");
+
+    let breakfastCancel = document.getElementById("breakfastCancel");
+
+    let breakfast = document.getElementById("breakfast");
+    let lunch = document.getElementById("lunch");
+    let dinner = document.getElementById("dinner");
+    let snack = document.getElementById("snack");
+
+    let breakfast2 = document.getElementById("breakfast2");
+
+    addBreakfastButton.addEventListener("click", function () {
+        breakfast.style.display = "none";
+        breakfast2.style.display = "inline-block";
+    });
+
+    cancelButton.addEventListener("click", function () {
+        breakfast.style.display = "inline-block";
+        breakfast2.style.display = "none";
+    });
+
+}
+
+window.onload = init;
